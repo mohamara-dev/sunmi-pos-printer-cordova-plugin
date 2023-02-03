6b1885c09346213699218cedfe2b0aa6e645c803
mohamara <m.desproda@gmail.com>
2023-02-03 07:31:24 +0330

    initial

---
+++ www/sunmiPOSPrinter.js
@@ -0,0 +1,36 @@
+cordova.define("org.cordova.plugin.sunmi.sunmiposprinter.sunmiPOSPrinter", function (require, exports, module) {
+    var exec = require('cordova/exec')
+
+    function PosPrinter() {
+
+    }
+    PosPrinter.prototype.printer = function (success, error, mainAction, action, message) {
+        exec(
+            function (result) {
+                success(result)
+            },
+
+            function (err) {
+                error(err)
+            },
+            'POSPrinter', mainAction, [action, message])
+    }
+
+    PosPrinter.prototype.printerMini = function (mainAction, action, message) {
+        exec(
+            function (result) {
+                alert(result)
+            },
+
+            function (err) {
+                alert(err)
+            },
+            'POSPrinter', mainAction, [action, message])
+    }
+
+
+    var posPrinter = new PosPrinter()
+    module.exports = posPrinter
+
+})
+
